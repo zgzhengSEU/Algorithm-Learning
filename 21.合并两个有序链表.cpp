ceb6bf7706244a988e2f346a1675298bd43e3abd
zgzhengSEU <zgzheng@vip.qq.com>
2021-08-08 19:26:29 +0800

    Update 21.合并两个有序链表.cpp

--- 21.合并两个有序链表.cpp
+++ 21.合并两个有序链表.cpp
@@ -64,7 +64,21 @@
 class Solution {
 public:
     ListNode* mergeTwoLists(ListNode* l1, ListNode* l2) {
-
+        if(!l1)return l2;
+        if(!l2)return l1;
+        ListNode *head=new ListNode,*cur1=l1,*cur2=l2,*cur=head;
+        while(cur1&&cur2){
+            if(cur1->val<=cur2->val){
+                cur->next=cur1;
+                cur1=cur1->next;
+            }else{
+                cur->next=cur2;
+                cur2=cur2->next;
+            }
+            cur=cur->next;
+        }
+        cur->next=cur1?cur1:cur2;
+        return head->next;
     }
 };
 // @lc code=end

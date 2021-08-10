54857dcd5371fe4a57f6ea907a32017e2837c36f
zgzhengSEU <zgzheng@vip.qq.com>
2021-08-10 18:14:13 +0800

    Create 206.反转链表.cpp

---
+++ 206.反转链表.cpp
@@ -0,0 +1,84 @@
+/*
+ * @lc app=leetcode.cn id=206 lang=cpp
+ *
+ * [206] 反转链表
+ *
+ * https://leetcode-cn.com/problems/reverse-linked-list/description/
+ *
+ * algorithms
+ * Easy (72.11%)
+ * Likes:    1891
+ * Dislikes: 0
+ * Total Accepted:    641.4K
+ * Total Submissions: 889.3K
+ * Testcase Example:  '[1,2,3,4,5]'
+ *
+ * 给你单链表的头节点 head ，请你反转链表，并返回反转后的链表。
+ * 
+ * 
+ * 
+ * 
+ * 示例 1：
+ * 
+ * 
+ * 输入：head = [1,2,3,4,5]
+ * 输出：[5,4,3,2,1]
+ * 
+ * 
+ * 示例 2：
+ * 
+ * 
+ * 输入：head = [1,2]
+ * 输出：[2,1]
+ * 
+ * 
+ * 示例 3：
+ * 
+ * 
+ * 输入：head = []
+ * 输出：[]
+ * 
+ * 
+ * 
+ * 
+ * 提示：
+ * 
+ * 
+ * 链表中节点的数目范围是 [0, 5000]
+ * -5000 
+ * 
+ * 
+ * 
+ * 
+ * 进阶：链表可以选用迭代或递归方式完成反转。你能否用两种方法解决这道题？
+ * 
+ * 
+ * 
+ */
+
+// @lc code=start
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* reverseList(ListNode* head) {
+        ListNode *cur=head,*prev=nullptr,*temp;
+        while(cur){
+            temp=cur;
+            cur=cur->next;
+            temp->next=prev;
+            prev=temp;
+        }
+        return prev;
+    }
+};
+// @lc code=end
+

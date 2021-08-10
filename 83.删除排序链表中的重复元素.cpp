2493d70f50beca9033e5a4a69d94f9f68ff6076d
zgzhengSEU <zgzheng@vip.qq.com>
2021-08-10 19:02:42 +0800

    Create 83.删除排序链表中的重复元素.cpp

---
+++ 83.删除排序链表中的重复元素.cpp
@@ -0,0 +1,75 @@
+/*
+ * @lc app=leetcode.cn id=83 lang=cpp
+ *
+ * [83] 删除排序链表中的重复元素
+ *
+ * https://leetcode-cn.com/problems/remove-duplicates-from-sorted-list/description/
+ *
+ * algorithms
+ * Easy (53.84%)
+ * Likes:    624
+ * Dislikes: 0
+ * Total Accepted:    291.7K
+ * Total Submissions: 541.7K
+ * Testcase Example:  '[1,1,2]'
+ *
+ * 存在一个按升序排列的链表，给你这个链表的头节点 head ，请你删除所有重复的元素，使每个元素 只出现一次 。
+ * 
+ * 返回同样按升序排列的结果链表。
+ * 
+ * 
+ * 
+ * 示例 1：
+ * 
+ * 
+ * 输入：head = [1,1,2]
+ * 输出：[1,2]
+ * 
+ * 
+ * 示例 2：
+ * 
+ * 
+ * 输入：head = [1,1,2,3,3]
+ * 输出：[1,2,3]
+ * 
+ * 
+ * 
+ * 
+ * 提示：
+ * 
+ * 
+ * 链表中节点数目在范围 [0, 300] 内
+ * -100 
+ * 题目数据保证链表已经按升序排列
+ * 
+ * 
+ */
+
+// @lc code=start
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* deleteDuplicates(ListNode* head) {
+        ListNode *cur=head,*dummyH=new ListNode(0,head),*prev=head;
+        while(cur&&cur->next){
+            cur=cur->next;
+            while(cur&&cur->val==prev->val){
+                prev->next=cur->next;
+                cur=cur->next;
+            }
+            prev=prev->next;
+        }
+        return dummyH->next;
+    }
+};
+// @lc code=end
+

99c922a3af1f0756331a24e100f2360c45429b16
zgzhengSEU <zgzheng@vip.qq.com>
2021-08-19 01:15:32 +0800

    Create 136.只出现一次的数字.cpp

---
+++ 136.只出现一次的数字.cpp
@@ -0,0 +1,45 @@
+/*
+ * @lc app=leetcode.cn id=136 lang=cpp
+ *
+ * [136] 只出现一次的数字
+ *
+ * https://leetcode-cn.com/problems/single-number/description/
+ *
+ * algorithms
+ * Easy (71.82%)
+ * Likes:    1971
+ * Dislikes: 0
+ * Total Accepted:    467.3K
+ * Total Submissions: 650.3K
+ * Testcase Example:  '[2,2,1]'
+ *
+ * 给定一个非空整数数组，除了某个元素只出现一次以外，其余每个元素均出现两次。找出那个只出现了一次的元素。
+ * 
+ * 说明：
+ * 
+ * 你的算法应该具有线性时间复杂度。 你可以不使用额外空间来实现吗？
+ * 
+ * 示例 1:
+ * 
+ * 输入: [2,2,1]
+ * 输出: 1
+ * 
+ * 
+ * 示例 2:
+ * 
+ * 输入: [4,1,2,1,2]
+ * 输出: 4
+ * 
+ */
+
+// @lc code=start
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+        int ret=0;
+        for(auto it:nums)ret^=it;
+        return ret;
+    }
+};
+// @lc code=end
+

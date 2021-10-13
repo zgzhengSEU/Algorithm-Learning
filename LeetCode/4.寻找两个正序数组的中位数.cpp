2de3e181c3fcca7176ebed0b0621e44cd2346213
SEUGarfield <zgzheng@vip.qq.com>
2021-10-13 19:46:04 +0800

    Create 4.寻找两个正序数组的中位数.cpp

---
+++ LeetCode/4.寻找两个正序数组的中位数.cpp
@@ -0,0 +1,102 @@
+/*
+ * @lc app=leetcode.cn id=4 lang=cpp
+ *
+ * [4] 寻找两个正序数组的中位数
+ *
+ * https://leetcode-cn.com/problems/median-of-two-sorted-arrays/description/
+ *
+ * algorithms
+ * Hard (40.89%)
+ * Likes:    4561
+ * Dislikes: 0
+ * Total Accepted:    524.9K
+ * Total Submissions: 1.3M
+ * Testcase Example:  '[1,3]\n[2]'
+ *
+ * 给定两个大小分别为 m 和 n 的正序（从小到大）数组 nums1 和 nums2。请你找出并返回这两个正序数组的 中位数 。
+ * 
+ * 
+ * 
+ * 示例 1：
+ * 
+ * 
+ * 输入：nums1 = [1,3], nums2 = [2]
+ * 输出：2.00000
+ * 解释：合并数组 = [1,2,3] ，中位数 2
+ * 
+ * 
+ * 示例 2：
+ * 
+ * 
+ * 输入：nums1 = [1,2], nums2 = [3,4]
+ * 输出：2.50000
+ * 解释：合并数组 = [1,2,3,4] ，中位数 (2 + 3) / 2 = 2.5
+ * 
+ * 
+ * 示例 3：
+ * 
+ * 
+ * 输入：nums1 = [0,0], nums2 = [0,0]
+ * 输出：0.00000
+ * 
+ * 
+ * 示例 4：
+ * 
+ * 
+ * 输入：nums1 = [], nums2 = [1]
+ * 输出：1.00000
+ * 
+ * 
+ * 示例 5：
+ * 
+ * 
+ * 输入：nums1 = [2], nums2 = []
+ * 输出：2.00000
+ * 
+ * 
+ * 
+ * 
+ * 提示：
+ * 
+ * 
+ * nums1.length == m
+ * nums2.length == n
+ * 0 
+ * 0 
+ * 1 
+ * -10^6 
+ * 
+ * 
+ * 
+ * 
+ * 进阶：你能设计一个时间复杂度为 O(log (m+n)) 的算法解决此问题吗？
+ * 
+ */
+
+// @lc code=start
+class Solution {
+public:
+    double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
+        int total = nums1.size() + nums2.size();
+        if (total % 2 == 0) {
+            int left = findKthNumber(nums1, 0, nums2, 0, total / 2);
+            int right = findKthNumber(nums1, 0, nums2, 0, total / 2 + 1);
+            return (left + right) / 2.0;
+        } else {
+            return findKthNumber(nums1, 0, nums2, 0, total / 2 + 1);
+        }
+    }
+    int findKthNumber(vector<int> &nums1, int i, vector<int> &nums2, int j, int k) {
+        if (nums1.size() - i > nums2.size() - j) return findKthNumber(nums2, j, nums1, i, k);
+        if (nums1.size() == i) return nums2[j + k - 1];
+        if (k == 1) return min(nums1[i], nums2[j]);
+        int si = min(i + k/2, (int)nums1.size()), sj = j + k - k/2;
+        if (nums1[si - 1] > nums2[sj - 1]) {
+            return findKthNumber(nums1, i, nums2, sj, k - (sj - j));
+        } else {
+            return findKthNumber(nums1, si, nums2, j, k - (si - i));
+        }
+    }
+};
+// @lc code=end
+

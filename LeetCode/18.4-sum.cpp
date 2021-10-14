521a6ba19b8e475e4cf0b1d6583929312e7c3b81
薛定谔的加菲猫 <zgzheng@vip.qq.com>
2021-10-14 22:20:44 +0800

    Create 18.4-sum.cpp

---
+++ LeetCode/18.4-sum.cpp
@@ -0,0 +1,76 @@
+/*
+ * @lc app=leetcode id=18 lang=cpp
+ *
+ * [18] 4Sum
+ *
+ * https://leetcode.com/problems/4sum/description/
+ *
+ * algorithms
+ * Medium (36.66%)
+ * Likes:    4607
+ * Dislikes: 544
+ * Total Accepted:    493.2K
+ * Total Submissions: 1.3M
+ * Testcase Example:  '[1,0,-1,0,-2,2]\n0'
+ *
+ * Given an array nums of n integers, return an array of all the unique
+ * quadruplets [nums[a], nums[b], nums[c], nums[d]] such that:
+ * 
+ * 
+ * 0 <= a, b, c, d < n
+ * a, b, c, and d are distinct.
+ * nums[a] + nums[b] + nums[c] + nums[d] == target
+ * 
+ * 
+ * You may return the answer in any order.
+ * 
+ * 
+ * Example 1:
+ * 
+ * 
+ * Input: nums = [1,0,-1,0,-2,2], target = 0
+ * Output: [[-2,-1,1,2],[-2,0,0,2],[-1,0,0,1]]
+ * 
+ * 
+ * Example 2:
+ * 
+ * 
+ * Input: nums = [2,2,2,2,2], target = 8
+ * Output: [[2,2,2,2]]
+ * 
+ * 
+ * 
+ * Constraints:
+ * 
+ * 
+ * 1 <= nums.length <= 200
+ * -10^9 <= nums[i] <= 10^9
+ * -10^9 <= target <= 10^9
+ * 
+ * 
+ */
+
+// @lc code=start
+class Solution {
+public:
+    vector<vector<int>> fourSum(vector<int>& nums, int target) {
+        sort(nums.begin(), nums.end());
+        vector<vector<int>> res;
+        for (int i = 0; i < nums.size(); ++i) {
+            if (i && nums[i] == nums[i - 1]) continue;
+            for (int j = i + 1; j < nums.size(); ++j) {
+                if (j > i + 1 && nums[j] == nums[j - 1]) continue;
+                for (int k = j + 1, u = nums.size() - 1; k < u; ++k) {
+                    if (k > j + 1 && nums[k] == nums[k - 1]) continue;
+                    while (u - 1 > k && nums[i] + nums[j] >= target - nums[k] - nums[u - 1]) --u;
+                    if (nums[i] + nums[j] == target - nums[k] - nums[u]) {
+                        res.push_back({nums[i], nums[j], nums[k], nums[u]});
+                    }
+                }
+            }
+        }
+        return res;
+    }
+};
+// @lc code=end
+

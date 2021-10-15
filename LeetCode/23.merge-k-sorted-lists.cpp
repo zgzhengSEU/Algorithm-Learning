7c0da98215b0988d5bd87a63d4524a4d8dc71a2f
SEUGarfield <zgzheng@vip.qq.com>
2021-10-15 22:39:25 +0800

    Create 23.merge-k-sorted-lists.cpp

---
+++ LeetCode/23.merge-k-sorted-lists.cpp
@@ -0,0 +1,95 @@
+/*
+ * @lc app=leetcode id=23 lang=cpp
+ *
+ * [23] Merge k Sorted Lists
+ *
+ * https://leetcode.com/problems/merge-k-sorted-lists/description/
+ *
+ * algorithms
+ * Hard (44.83%)
+ * Likes:    8904
+ * Dislikes: 392
+ * Total Accepted:    1M
+ * Total Submissions: 2.3M
+ * Testcase Example:  '[[1,4,5],[1,3,4],[2,6]]'
+ *
+ * You are given an array of k linked-lists lists, each linked-list is sorted
+ * in ascending order.
+ *
+ * Merge all the linked-lists into one sorted linked-list and return it.
+ *
+ *
+ * Example 1:
+ *
+ *
+ * Input: lists = [[1,4,5],[1,3,4],[2,6]]
+ * Output: [1,1,2,3,4,4,5,6]
+ * Explanation: The linked-lists are:
+ * [
+ * ⁠ 1->4->5,
+ * ⁠ 1->3->4,
+ * ⁠ 2->6
+ * ]
+ * merging them into one sorted list:
+ * 1->1->2->3->4->4->5->6
+ *
+ *
+ * Example 2:
+ *
+ *
+ * Input: lists = []
+ * Output: []
+ *
+ *
+ * Example 3:
+ *
+ *
+ * Input: lists = [[]]
+ * Output: []
+ *
+ *
+ *
+ * Constraints:
+ *
+ *
+ * k == lists.length
+ * 0 <= k <= 10^4
+ * 0 <= lists[i].length <= 500
+ * -10^4 <= lists[i][j] <= 10^4
+ * lists[i] is sorted in ascending order.
+ * The sum of lists[i].length won't exceed 10^4.
+ *
+ *
+ */
+
+// @lc code=start
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+ public:
+  struct cmp {
+    bool operator()(ListNode* a, ListNode* b) { return a->val > b->val; }
+  };
+
+  ListNode* mergeKLists(vector<ListNode*>& lists) {
+      auto dummy = new ListNode(-1), tail = dummy;
+      priority_queue<ListNode*, vector<ListNode*>, cmp> heap;
+      for (auto l : lists) if (l) heap.push(l);
+      while (heap.size()) {
+          auto t = heap.top();
+          heap.pop();
+          if (t->next) heap.push(t->next);
+          tail = tail->next = t;
+      }
+      return dummy->next;
+  }
+};
+// @lc code=end

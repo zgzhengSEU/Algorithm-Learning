8f290b2e8e27cd2673a8ac09e96f8be7ba3d5731
薛定谔的加菲猫 <zgzheng@vip.qq.com>
2021-10-15 17:34:32 +0800

    Create 22.generate-parentheses.cpp

---
+++ LeetCode/22.generate-parentheses.cpp
@@ -0,0 +1,44 @@
+/*
+ * @lc app=leetcode id=22 lang=cpp
+ *
+ * [22] Generate Parentheses
+ *
+ * https://leetcode.com/problems/generate-parentheses/description/
+ *
+ * algorithms
+ * Medium (68.16%)
+ * Likes:    10077
+ * Dislikes: 403
+ * Total Accepted:    876.2K
+ * Total Submissions: 1.3M
+ * Testcase Example:  '3'
+ *
+ * Given n pairs of parentheses, write a function to generate all combinations
+ * of well-formed parentheses.
+ * 
+ * 
+ * Example 1:
+ * Input: n = 3
+ * Output: ["((()))","(()())","(())()","()(())","()()()"]
+ * Example 2:
+ * Input: n = 1
+ * Output: ["()"]
+ * 
+ * 
+ * Constraints:
+ * 
+ * 
+ * 1 <= n <= 8
+ * 
+ * 
+ */
+
+// @lc code=start
+class Solution {
+public:
+    vector<string> generateParenthesis(int n) {
+        
+    }
+};
+// @lc code=end
+

6f613dc0fdfe71ceba4090098d85169b98955435
SEUGarfield <zgzheng@vip.qq.com>
2021-10-22 09:47:11 +0800

    Create 35.search-insert-position.cpp

---
+++ LeetCode/35.search-insert-position.cpp
@@ -0,0 +1,65 @@
+/*
+ * @lc app=leetcode id=35 lang=cpp
+ *
+ * [35] Search Insert Position
+ *
+ * https://leetcode.com/problems/search-insert-position/description/
+ *
+ * algorithms
+ * Easy (42.59%)
+ * Likes:    5065
+ * Dislikes: 327
+ * Total Accepted:    1M
+ * Total Submissions: 2.4M
+ * Testcase Example:  '[1,3,5,6]\n5'
+ *
+ * Given a sorted array of distinct integers and a target value, return the
+ * index if the target is found. If not, return the index where it would be if
+ * it were inserted in order.
+ * 
+ * You must write an algorithm with O(log n) runtime complexity.
+ * 
+ * 
+ * Example 1:
+ * Input: nums = [1,3,5,6], target = 5
+ * Output: 2
+ * Example 2:
+ * Input: nums = [1,3,5,6], target = 2
+ * Output: 1
+ * Example 3:
+ * Input: nums = [1,3,5,6], target = 7
+ * Output: 4
+ * Example 4:
+ * Input: nums = [1,3,5,6], target = 0
+ * Output: 0
+ * Example 5:
+ * Input: nums = [1], target = 0
+ * Output: 0
+ * 
+ * 
+ * Constraints:
+ * 
+ * 
+ * 1 <= nums.length <= 10^4
+ * -10^4 <= nums[i] <= 10^4
+ * nums contains distinct values sorted in ascending order.
+ * -10^4 <= target <= 10^4
+ * 
+ * 
+ */
+
+// @lc code=start
+class Solution {
+public:
+    int searchInsert(vector<int>& nums, int target) {
+        int l = 0, r = nums.size();
+        while (l < r) {
+            int mid = l + r >> 1;
+            if (nums[mid] >= target) r = mid;
+            else l = mid + 1;
+        }
+        return l;
+    }
+};
+// @lc code=end
+

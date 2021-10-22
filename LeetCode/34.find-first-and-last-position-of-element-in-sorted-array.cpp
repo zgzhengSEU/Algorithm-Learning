e561f65bb040c251e5c56579a4b5861d1ab4e78b
SEUGarfield <zgzheng@vip.qq.com>
2021-10-22 09:47:09 +0800

    Create 34.find-first-and-last-position-of-element-in-sorted-array.cpp

---
+++ LeetCode/34.find-first-and-last-position-of-element-in-sorted-array.cpp
@@ -0,0 +1,62 @@
+/*
+ * @lc app=leetcode id=34 lang=cpp
+ *
+ * [34] Find First and Last Position of Element in Sorted Array
+ *
+ * https://leetcode.com/problems/find-first-and-last-position-of-element-in-sorted-array/description/
+ *
+ * algorithms
+ * Medium (38.84%)
+ * Likes:    7670
+ * Dislikes: 244
+ * Total Accepted:    856.3K
+ * Total Submissions: 2.2M
+ * Testcase Example:  '[5,7,7,8,8,10]\n8'
+ *
+ * Given an array of integers nums sorted in ascending order, find the starting
+ * and ending position of a given target value.
+ * 
+ * If target is not found in the array, return [-1, -1].
+ * 
+ * You must write an algorithm with O(log n) runtime complexity.
+ * 
+ * 
+ * Example 1:
+ * Input: nums = [5,7,7,8,8,10], target = 8
+ * Output: [3,4]
+ * Example 2:
+ * Input: nums = [5,7,7,8,8,10], target = 6
+ * Output: [-1,-1]
+ * Example 3:
+ * Input: nums = [], target = 0
+ * Output: [-1,-1]
+ * 
+ * 
+ * Constraints:
+ * 
+ * 
+ * 0 <= nums.length <= 10^5
+ * -10^9 <= nums[i] <= 10^9
+ * nums is a non-decreasing array.
+ * -10^9 <= target <= 10^9
+ * 
+ * 
+ */
+
+// @lc code=start
+class Solution {
+public:
+    vector<int> searchRange(vector<int>& nums, int target) {
+        int l = 0 , r = nums.size() - 1;
+        while (l < r) {
+            int mid = l + r >> 1;
+            if (nums[mid] >= target) r = mid;
+            else l = mid + 1;
+        }
+        if (l > r || nums[l] != target) return {-1, -1};
+        while (r + 1 < nums.size() && nums[r + 1] == nums[l]) ++r;
+        return {l, r};
+    }
+};
+// @lc code=end
+
